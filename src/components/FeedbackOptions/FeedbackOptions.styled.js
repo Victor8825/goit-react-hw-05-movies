4b9b3995fadbf7c340f2cc56a8f2f6040b012cba
Victor8825 <victorrv2588@gmail.com>
2022-08-14 10:59:31 +0300

    add styles to button

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -3,5 +3,11 @@ import styled from 'styled-components';
 export const StyledFeedbackOptions = styled.div`
   button {
     border-radius: ${p => p.theme.radii.sm};
+    border: 1px solid grey;
+  }
+
+  button:hover {
+    background-color: #42aaf5;
+    border: ${p => p.theme.borders.none};
   }
 `;
