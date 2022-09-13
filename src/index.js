48bd159e4e3400a8f124e9fac669f40d2881dc02
Victor8825 <victorrv2588@gmail.com>
2022-09-13 23:13:06 +0300

    add react strict mode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <BrowserRouter basename="/goit-react-hw-05-movies/">
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
